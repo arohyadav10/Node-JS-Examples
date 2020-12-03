0f98a4cacd27e5cc6260ccace718b26f6d3958b3
arohyadav10 <arohyadav8@gmail.com>
2020-12-03 15:46:26 +0530

    practise prograns

---
+++ expressstaticfiletutorial/index.js
@@ -0,0 +1,24 @@
+import express from "express" 
+import { join } from 'path' 
+const app = express() 
+
+var PORT = process.env.port || 3000 
+
+// View Engine Setup 
+app.set("views", join(__dirname)) 
+app.set("view engine", "ejs") 
+
+app.get("/mysubmit", function(req, res){ 
+
+	var name = req.query.name 
+	var age = req.query.age 
+	
+	console.log("Name :", name) 
+	console.log("Age :", age) 
+	res.send("welcome " +name+" and your age is"+age )
+}) 
+
+app.listen(PORT, function(error){ 
+	if(error) throw error 
+	console.log("Server created Successfully on PORT", PORT) 
+}) 
